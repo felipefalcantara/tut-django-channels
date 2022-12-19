fccdc5474a0fdce93030ca5ebf44d9aeb79adc13
felipefalcantara <felipefr.alcantara@gmail.com>
2022-12-19 15:45:55 -0300

    Part 2: First consumer

--- channels_site/channels_site/asgi.py
+++ channels_site/channels_site/asgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_site.settings')
@@ -18,7 +20,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_site.settings')
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can ad other protocols later.)
-})
+import chat.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
